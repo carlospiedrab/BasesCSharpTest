9242ec60ac51f399c588013e6a56feca820fd67d
Carlos Piedra <barce90@hotmail.com>
2024-08-20 19:59:13 -0400

    Direccion Completa

--- introduccion/obj/Debug/net8.0/introduccion.AssemblyInfo.cs
+++ introduccion/obj/Debug/net8.0/introduccion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("introduccion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fe11fdcbfebd29009d6c18e1dfe8974c41ef4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d2dc8fd6dbcdaf0bb904e357ccc8bb7d8f009e")]
 [assembly: System.Reflection.AssemblyProductAttribute("introduccion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("introduccion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
